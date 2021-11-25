3712a0498b1315a925ad14e944682921a12c7edd
HoangNam1910 <18521120@gm.uit.edu.vn>
2021-11-25 22:17:53 +0700

    commit Models FavoriteDetail

---
+++ Models/FavoriteDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sky.Models
+{
+    [Table("FavoriteDetail")]
+    public class FavoriteDetail
+    {
+        public int FavoriteId { set; get; }
+        public int ProductId { set; get; }
+
+
+        //
+
+
+        public Favorite Favorite { set; get; }
+        public Product Product { set; get; }
+    }
+}
