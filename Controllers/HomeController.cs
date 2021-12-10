f71b35f45b27d43d89e7b7bac453dc5cf24e9d5a
tnvinh00 <tnvinh99@gmail.com>
2021-12-10 13:16:14 +0700

    add code home control

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,118 @@ namespace Sky.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly SkyAppDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger,
+            IWebHostEnvironment hostEnvironment,
+            SkyAppDbContext context)
+        {
+            _logger = logger;
+            _hostEnvironment = hostEnvironment;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            string path = Path.Combine(_hostEnvironment.WebRootPath, "Panel");
+
+            string[] filePaths1 = Directory.GetFiles(path, "*.png");
+
+            string[] filePaths2 = Directory.GetFiles(path, "*.jpg");
+
+            string[] filePaths3 = Directory.GetFiles(path, "*.jpeg");
+
+            var filePaths = filePaths1.Union(filePaths2).Union(filePaths3).ToArray();
+
+            List<string> img = new List<string>();
+
+            foreach (var i in filePaths)
+            {
+                string temp = Path.GetFileName(i);
+                img.Add(temp);
+            }
+
+            ViewBag.ImgList = img;
+
+            return View();
+        }
+
+        [NoDirectAccess]
+        public IActionResult Menu()
+        {
+            var sky = _context.CategorieDbSet.Select(c => c.CategoryName).ToList();
+
+            sky.Remove("Nam");
+            sky.Remove("Nữ");
+
+            ViewBag.menu = sky;
+
+            return View();
+        }
+
+        public IActionResult BestSale()
+        {
+            var sale = _context.OrderDetailDbSet
+                .GroupBy(c => c.ProductId)
+                .Select(a => new { ProductId = a.Key, SaleCount = a.Count() })
+
+                .OrderByDescending(x => x.SaleCount).Take(10);
+
+            var product = _context.ProductDbSet
+                .Join(sale,
+                    a => a.ProductId,
+                    b => b.ProductId,
+                    (a, b) => a
+                )
+                .ToList();
+
+            return View(product);
+        }
+
+        [NoDirectAccess]
+        public PartialViewResult BestView()
+        {
+            var best = _context.ProductDbSet.OrderByDescending(c => c.ViewCount).Take(10);
+
+            return PartialView(best.ToList());
+        }
+
+        [NoDirectAccess]
+        public IActionResult BestFavorite()
+        {
+            var favo = _context.FavoriteDetailDbSet
+                .GroupBy(c => c.ProductId)
+                .Select(a => new { ProductId = a.Key, FavoCount = a.Count() })
+
+                .OrderByDescending(x => x.FavoCount).Take(10);
+
+            var product = _context.ProductDbSet
+                .Join(favo,
+                    a => a.ProductId,
+                    b => b.ProductId,
+                    (a, b) => a
+                )
+                .ToList();
+
+            return View(product.ToList());
+        }
+            
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Errors()
+        {
+            return View();
+        }
     }
 }
---
+++ Data/SkyAppDbContext.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Type = Sky.Models.Type;
+
+namespace Sky.Data
+{
+    public class SkyAppDbContext : DbContext
+    {
+        public SkyAppDbContext(DbContextOptions<SkyAppDbContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //mỗi giỏ hàng có nhiêu chi tiết GH, mỗi Sản phẩm có nhiều chi tiết GH
+            builder.Entity<CartDetail>()
+                .HasKey(pk => new { pk.CartId, pk.ProductId });
+            builder.Entity<CartDetail>()
+                .HasOne(c => c.Cart)
+                .WithMany(cd => cd.CartDetails)
+                .HasForeignKey(pk => pk.CartId);
+            builder.Entity<CartDetail>()
+                .HasOne(c => c.Product)
+                .WithMany(cd => cd.CartDetails)
+                .HasForeignKey(fk => fk.ProductId);
+
+            //mỗi yêu thích có nhiêu chi tiết yêu thích, mỗi Sản phẩm có nhiều chi tiết yêu thích
+            builder.Entity<FavoriteDetail>()
+                .HasKey(pk => new { pk.FavoriteId, pk.ProductId });
+            builder.Entity<FavoriteDetail>()
+                .HasOne(f => f.Favorite)
+                .WithMany(fd => fd.FavoriteDetails)
+                .HasForeignKey(pk => pk.FavoriteId);
+            builder.Entity<FavoriteDetail>()
+                .HasOne(f => f.Product)
+                .WithMany(fd => fd.FavoriteDetails)
+                .HasForeignKey(fk => fk.ProductId);
+
+            //mỗi đơn hàng có nhiêu chi tiết DH, mỗi Sản phẩm có nhiều chi tiết DH
+            builder.Entity<OrderDetail>()
+                .HasKey(pk => new { pk.OrderId, pk.ProductId });
+            builder.Entity<OrderDetail>()
+                .HasOne(o => o.Order)
+                .WithMany(od => od.OrderDetails)
+                .HasForeignKey(pk => pk.OrderId);
+            builder.Entity<OrderDetail>()
+                .HasOne(o => o.Product)
+                .WithMany(od => od.OrderDetails)
+                .HasForeignKey(fk => fk.ProductId);
+
+
+            ////////////////////////////////////////////////////////////////////
+            //Mỗi user có 1 nhiều đơn hàng
+            //builder.Entity<Order>()
+            //    .HasOne(o => o.SkyUser)
+            //    .WithMany(u => u.Orders)
+            //    .HasForeignKey(fk => fk.UserId);
+
+            ////Mỗi user có 1 giỏ hàng
+            //builder.Entity<SkyUser>()
+            //    .HasOne(c => c.Cart)
+            //    .WithOne(u => u.SkyUser)
+            //    .HasForeignKey<Cart>(fk => fk.UserId);
+
+            ////Mỗi user có 1 ds yêu thích
+            //builder.Entity<SkyUser>()
+            //    .HasOne(c => c.Favorite)
+            //    .WithOne(u => u.SkyUser)
+            //    .HasForeignKey<Favorite>(fk => fk.UserId);
+
+
+            ////////////////////////////////////////////////////////////////////
+            //Mỗi sản phẩm có 1 danh mục
+            builder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(fk => fk.CategoryId);
+
+            //Mỗi sản phẩm có 1 loại
+            builder.Entity<Product>()
+                .HasOne(p => p.Type)
+                .WithMany(c => c.Products)
+                .HasForeignKey(fk => fk.TypeId);
+
+
+            // Init data
+            builder.Entity<Cart>()
+                .HasData(new Cart() { CartId = 1, UserId = "b74ddd14-6340-4840-95c2-db12554843e5" });
+            builder.Entity<Favorite>()
+                .HasData(new Favorite() { FavoriteId = 1, UserId = "b74ddd14-6340-4840-95c2-db12554843e5" });
+
+            base.OnModelCreating(builder);
+        }
+
+
+
+        public DbSet<Product> ProductDbSet { get; set; }
+        public DbSet<Type> TypeDbSet { get; set; }
+        public DbSet<Category> CategorieDbSet { get; set; }
+        public DbSet<Cart> CartDbSet { get; set; }
+        public DbSet<CartDetail> CartDetailDbSet { get; set; }
+        public DbSet<Order> OrderDbSet { get; set; }
+        public DbSet<OrderDetail> OrderDetailDbSet { get; set; }
+        public DbSet<Favorite> FavoriteDbSet { get; set; }
+        public DbSet<FavoriteDetail> FavoriteDetailDbSet { get; set; }
+    }
+}
