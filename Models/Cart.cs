07c49a6a0156b57bf2d0e8ae61f4fe203ba4187f
tnvinh00 <tnvinh99@gmail.com>
2021-12-10 13:15:17 +0700

    add more model

---
+++ Models/Cart.cs
@@ -0,0 +1,28 @@
+﻿using Sky.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sky.Models
+{
+    [Table("Cart")]
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+
+
+
+
+        [Required]
+        public string UserId { get; set; }
+
+
+
+        public List<CartDetail> CartDetails { set; get; }
+
+    }
+}
